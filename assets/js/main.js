c844e74cd09b21a2d526de2b20ea775373bc8ae1
Kurokabe <farid.abdalla.13@gmail.com>
2023-03-18 18:49:58 +0100

    Removed kaggle link for now

--- assets/js/main.js
+++ assets/js/main.js
@@ -135,8 +135,8 @@
     new Typed('.typed', {
       strings: typed_strings,
       loop: true,
-      typeSpeed: 100,
-      backSpeed: 50,
+      typeSpeed: 40,
+      backSpeed: 20,
       backDelay: 2000
     });
   }
